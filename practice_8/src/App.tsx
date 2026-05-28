abb68f464c0190576dc2fef9d52ae60150320418
Bradley Caliva <bcaliva21@gmail.com>
2026-05-28 14:32:26 -0700

    feat: practice_8

--- practice_8/src/App.tsx
+++ practice_8/src/App.tsx
@@ -1,121 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React, { useState } from 'react'
+import './styles.css'
+
+
+const HTML_CONTENT = `
+  The HyperText Markup Language or HTML is the standard markup language
+  for documents designed to be displayed in a web browser.
+`;
+const CSS_CONTENT = `
+  Cascading Style Sheets is a style sheet language used for describing
+  the presentation of a document written in a markup language such as
+  HTML or XML.
+`;
+
+const JAVASCRIPT_CONTENT = `
+  JavaScript, often abbreviated as JS, is a programming language that is
+  one of the core technologies of the World Wide Web, alongside HTML and
+  CSS.
+`;
+
+function Accordion() {
+  return (
+    <div>
+      <h1>Title</h1>
+      <div className="sections">
+        <Section content={HTML_CONTENT} title={"HTML"} />
+        <Section content={CSS_CONTENT} title={"CSS"} />
+        <Section content={JAVASCRIPT_CONTENT} title={"JavaScript"} />
+      </div>
+    </div>
+  );
+}
+
+function Section({ content, title }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div>
+      <button
+        className="section-title"
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        <h2>
+          {title}{" "}
+          <span
+            aria-hidden={true}
+            className={
+              isExpanded ? "accordion-icon--rotated" : "accordion-icon"
+            }
+          />
+        </h2>
+      </button>
+      {isExpanded && <div>{content}</div>}
+    </div>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <Accordion />
   )
 }
 
